209523bc05aa723ae2d4da30d4b2551d21ddf1aa
Mohammed Khaled <kingm7md112@gmail.com>
2022-09-16 15:55:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal Portfolio to showcase my work
+
+Plans and ideas:
+
+- Include mutiple color themes for the user to choose from
+- Utilize a glass modern design
+- Visual heavy interface
+- Make it your best work yet
---
+++ app.js
@@ -0,0 +1,14 @@
+let toTop = document.getElementById("top");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
